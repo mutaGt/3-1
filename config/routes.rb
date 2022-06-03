16992762ddf030c1476bd55ad59b7827e0fe0654
mutaGt <104976989+mutaGt@users.noreply.github.com>
2022-06-03 00:52:47 +0000

    [Finish]3-4

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   get 'lists/new'
   post'lists'=>'lists#create'
-  get 'lists/show'
   get 'lists/edit'
   get '/top'=>'homes#top'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
@@ -9,4 +8,5 @@ Rails.application.routes.draw do
   get 'top'=>'homes#top'
   post 'lists'=>'lists#create'
   get 'lists'=>'lists#index'
+  get 'lists/:id'=>'lists#show',as:'list'
 end
